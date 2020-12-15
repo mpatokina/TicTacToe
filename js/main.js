6a38eadbe84121888ee8bb0ad5c3a80ddc2d044d
Maria Patokina <mpatokina@hotmail.com>
2020-12-14 21:01:56 -0500

    my third commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
-const statusDisplay = document.querySelector('.gameStatus');
+const statusDisplay = document.querySelector('.gameStatus'); //stores game status
 let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
-const winningMessage = () => `Congrats, Player ${currentPlayer}`;
-const tieMessage = () => `It's a tie!`;
-const currentPlayerTurn = () => `it's ${currentPlayer}'s turn`;
 
-statusDisplay.innerHTML = currentPlayerTurn();
+let currentPlayer = "X"; // stores current player
 
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick));
-document.querySelector('.newGame').addEventListener('click', restartGame);
+let board = []; // sets the board as an array with nine elements each = ""
+for (let i = 0; i <= 8; i++) {
+    board.push("");
+}
+
+winningMessage = function() { // declares winning message
+    return `Congrats, Player ${currentPlayer} !`;
+}
+tieMessage = function() { // declares tie message
+    return `It's a Tie!`;
+}
+currentPlayerTurn = function() { // declares whose turn message
+    return `It's ${currentPlayer}'s turn`;
+}
+
+statusDisplay.innerHTML = currentPlayerTurn(); // initial message to show whose turn it is 
+
+//document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick)); // listeners to game cells
+document.querySelectorAll('.cell').forEach(function(cell) {
+    cell.addEventListener('click', cellClick);
+})
+
+
+
+document.querySelector('.newGame').addEventListener('click', restartGame); // restart button
 
 function cellClick(clickedCellEvent) {
 
-    const clickedCell = clickedCellEvent.target;
+    const clickedCell = clickedCellEvent.target; // saves clicked html in a variable
 
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('cell'));
-    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index')); // grabs the 'data-cell-index' attribute from clicked cell
+    if (board[clickedCellIndex] !== "" || !gameActive) {
         return;
     }
     cellPlayed(clickedCell, clickedCellIndex);
     resultValidation();
-
 }
+
 function cellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
+    board[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
 }
-    const winningConditions = [
+    const winningConditions = [ // stores all winning conditions
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -38,13 +56,13 @@ function cellPlayed(clickedCell, clickedCellIndex) {
         [2, 4, 6]
     ];
 
-    function resultValidation() {
+    function resultValidation() { // checks for winning conditions
         let roundWon = false;
         for (let i = 0; i <= 7; i++) {
             const winCondition = winningConditions[i];
-            let x = gameState[winCondition[0]];
-            let y = gameState[winCondition[1]];
-            let z = gameState[winCondition[2]];
+            let x = board[winCondition[0]];
+            let y = board[winCondition[1]];
+            let z = board[winCondition[2]];
             if (x === '' || y === '' || z === '') {
                 continue;
             }
@@ -54,27 +72,36 @@ function cellPlayed(clickedCell, clickedCellIndex) {
             }
         }
     if (roundWon) {
-            statusDisplay.innerHTML = winningMessage();
+            statusDisplay.innerHTML = winningMessage(); // shows winning message
             gameActive = false;
             return;
         }
-    let roundTie = !gameState.includes("");
+    let roundTie = !board.includes("");
     if (roundTie) {
-       statusDisplay.innerHTML = tieMessage();
+       statusDisplay.innerHTML = tieMessage(); // shows tie message
        gameActive = false;
        return;
     }
     playerChange();
 }
-function playerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
+
+function playerChange() {// changes players turns
+    if (currentPlayer === "X"){
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
+    statusDisplay.innerHTML = currentPlayerTurn(); // shows player turn
 }
 
-function restartGame() {
+function restartGame() { // restarts game
     gameActive = true;
     currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
+    board = []; // resets the board to ""
+    for (let i = 0; i <= 8; i++) {
+    board.push("");
+    }
     statusDisplay.innerHTML = currentPlayerTurn();
     document.querySelectorAll('.cell')
                 .forEach(cell => cell.innerHTML = "");
